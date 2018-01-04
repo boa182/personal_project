a21576f89a4439f6d5a28c6a0bc4ca684e8474fa
boa <281189459@qq.con>
2018-01-04 17:52:52 +0800

    发现头部的公共样式写在了index上，所以我要引入base.js，大修改前的版本

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,6 @@
 require(['config'],function(){
 	require(['common','jquery'],function(){
+		//1.表单的tab标签的切换
 		(function() {
 			var $tabTitle = $('.tab-title');
 			var $tabItem = $('.item');
@@ -20,6 +21,36 @@ require(['config'],function(){
 			})
 			
 		})();
+		//2.手机号的验证
+		(function(){
+			var $user = $('#phoneNum4QuickLogin');
+			
+			var $iempty = $('#phoneNum4QuickLogin_empty');
+			
+			function isMobilePhone(value) {
+				if(value.search(/^(\+\d{2,3})?\d{11}$/) == -1)
+					return false;
+				else
+					return true;
+			}
+			
+			function checkMobilePhone(telphone) {
+				if(telphone == '' || !isMobilePhone(telphone)) {
+					
+					
+					$iempty.show();
+				} else {
+					
+					
+					$iempty.hide();
+				}
+			}
+			
+			$user.on('blur', function() {
+				var telphone = this.value;
+				checkMobilePhone(telphone);
+			})
+		})()
 		
 	})
 })
---
+++ src/js/register.js
@@ -0,0 +1,71 @@
+require(['config'],function(){
+	require(['common','jquery'],function(){
+		
+		//1、用户名手机号码验证
+		(function(){
+			var $user = $('.u_name');
+			var $itrue = $('.phone .i_true');
+			var $iempty = $('.phone .i_empty');
+			function isMobilePhone(value) {
+				if(value.search(/^(\+\d{2,3})?\d{11}$/) == -1)
+					return false;
+				else
+					return true;
+			}
+			function checkMobilePhone(telphone) {
+				if(telphone == '' || !isMobilePhone(telphone)) {
+					$user.addClass('empty');
+					$itrue.hide();
+					$iempty.show();
+				} else {
+					$itrue.show();
+					$user.removeClass('empty');
+					$iempty.hide();
+				}
+			}
+			
+			$user.on('blur',function(){
+				var telphone = this.value;
+				checkMobilePhone(telphone);
+			})
+		})();
+		
+		//2用户密码验证
+		(function(){
+			var $user = $('.pw1');
+			var $pw2 = $('.pw2');
+			var $password_tip = $('#password_tip');
+			var $iempty = $('.pw .i_empty');
+			var $itrue = $('.pw .i_true');
+			function checkPwd1(pwd1) {
+				if(pwd1.search(/^.{6,20}$/) == -1) {
+					$user.addClass('error');
+					$iempty.show();
+					$itrue.hide();
+				} else {
+					$user.removeClass('error');
+					$iempty.hide();
+					$pw2.removeAttr("disabled");
+					$itrue.show();
+				}
+			}
+			
+			$user.on('focus',function(){
+				$password_tip.show();
+				$iempty.hide();
+				$itrue.hide();
+				
+			}).on('blur',function(){
+				$password_tip.hide();
+				var pwd1 = this.value;
+				checkPwd1(pwd1);
+			})
+			
+		})();
+		
+		
+		
+		
+	})
+	
+})
