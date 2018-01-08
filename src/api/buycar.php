9e4a41f78245dc6e2cdc7ff56917289a41832c69
boa <281189459@qq.con>
2018-01-08 14:22:57 +0800

    购物第二次大改前的保存

--- src/api/buycar.php
+++ src/api/buycar.php
@@ -1,35 +1,16 @@
 <?php
 	include 'connect.php';
 
-	$id = isset($_GET['id']) ? $_GET['id'] : '';
-	$id = json_decode($id);
+	// 获取参数
+	$id = isset($_GET['id'])? $_GET['id']:'';
 
-	//遍历数组，提取id
-	$a = array_map(function($items){
-		return $items->id;
-	}, $id);
-	// 组合成SQL语句
-	$a = implode(",", $a);
-	$a = '('.$a.')';
+	// 编写服务器查询语句
+	$sql = "select * from goodlists where gid='$id'";
 
+	// 新建查询,获取查询结果
+	$res = $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+
+	// 把结果返回前端，转化为json字符串
+	echo json_encode($res,JSON_UNESCAPED_UNICODE)
 	
-	// // 编写SQL语句
-	$sql = "select * from goodlists where id in".$a;
-
-	// 新建查询
-	$res = $conn->query($sql);
-
-	// 获取查询结果
-	$rows = $res->fetch_all(MYSQLI_ASSOC);
-
-	// 格式化数据
-    $result = array(
-    	'data'=>$rows,
-    );
-
-	echo json_encode($result,JSON_UNESCAPED_UNICODE);
-
-
-	//关闭连接
-	$conn->close();
 ?>
--- src/js/buycar.js
+++ src/js/buycar.js
@@ -6,6 +6,92 @@ require(['config'],function(){
 		//cookie和ajax请求
 		var cookieArr = com.getCookie('goodslist');
 		cookieArr = cookieArr ? JSON.parse(cookieArr) : [];
+		
+		if(cookieArr==[]){
+			$('.nothing').show();
+			$('.inner').hide();
+		}else{
+			$('.nothing').hide();
+			$('.inner').show();
+			var goodsId = [];
+			cookieArr.forEach(function(item){
+				goodsId.push(item.id);
+			});
+			ajz(goodsId);
+			console.log(goodsId);
+		};
+		function ajz(goodsId){
+			
+			var str ="";
+			
+			goodsId.forEach(function(item){
+				$.ajax({
+					type:"get",
+					url:"../api/buycar.php",
+					data:{
+						id:item
+					},
+					success:function(res){
+						console.log(JSON.parse(res));
+						var res = JSON.parse(res);
+						str+=`<tr id=${res[0].gid}>
+								<td><input type="radio" /></td>
+								<td><img src="${res[0].Pic180}"/><span>${res[0].name}</span></td>
+								<td>￥${res[0].MarketPrice}</td>
+								<td>
+									<button class="cut">-</button>
+									<input type="text" value="1"/>
+									<button class="push">+</button>
+								</td>
+								<td>0.04kg</td>
+								<td>有货</td>
+								<td>￥322.00</td>
+								<td>
+									<button>收藏</button>
+									<button class="del">删除</button>
+								</td>
+							</tr>`
+						$('tbody').html(str);
+						$('.del').on('click',function(){
+							var $tr = $(this).closest('tr');
+							var $curId = $tr.attr('id');
+							
+							cookieArr.forEach(function(items,i){
+								if($curId==items.id){
+									cookieArr.splice(i,1);
+		
+									
+								}
+							})
+							//重新写入删除后的id
+							base.setCookie('goodslist',cookieArr);
+							$tr.remove();
+						});
+						$('.push').on('click',function(){
+							var val = $(this).siblings('input');
+							val.val(function(a,b){
+								return b*1+1
+							})
+							
+						});
+						$('.cut').on('click',function(){
+							var val = $(this).siblings('input');
+							val.val(function(a,b){
+								if(b>0){
+									
+									return b*1-1
+								}else{
+									return 0
+								}
+							})
+						});
+					}
+				});
+			});
+		};
+		
+		
+		
 	});
 	
 });
